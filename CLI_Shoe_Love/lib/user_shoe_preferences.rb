e3a29986cc439286d5f00643a65c54ab657fad60
charityyoungblood <youngblood.charity@gmail.com>
2018-01-06 17:13:19 -0800

    added variables to shoe_questions to gather/store user input in user_shoe_preferences.rb

--- CLI_Shoe_Love/lib/user_shoe_preferences.rb
+++ CLI_Shoe_Love/lib/user_shoe_preferences.rb
@@ -6,11 +6,14 @@ class Shoes
     
   end 
   
-  def shoe_questions # this method should prompt the user with questions for shoe type (occassion), color and heel height
+  def shoe_questions # this method should prompt the user with questions for shoe type (occasion), color and heel height
     # this method will take in user input and return a list of three selections based on user preferences 
-    puts "What type of occassion are you shopping for?"
+    puts "What type of occasion are you shopping for?"
+      occasion = gets.chomp # save in a variable in case you need to operate on 
     puts "What color shoes would you prefer?"
+      shoe_color = gets.chomp
     puts "What is your ideal heel height?"
+      heel_height = gets.chomp
   end 
   
   
