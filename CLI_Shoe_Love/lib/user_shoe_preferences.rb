3d01d172a049c9bdc69e11942cd08db827aed3ee
charityyoungblood <youngblood.charity@gmail.com>
2018-01-06 17:19:59 -0800

    added specifics to shoe_questions method in user_shoe_preferences.rb

--- CLI_Shoe_Love/lib/user_shoe_preferences.rb
+++ CLI_Shoe_Love/lib/user_shoe_preferences.rb
@@ -8,11 +8,14 @@ class Shoes
   
   def shoe_questions # this method should prompt the user with questions for shoe type (occasion), color and heel height
     # this method will take in user input and return a list of three selections based on user preferences 
-    puts "What type of occasion are you shopping for?"
-      occasion = gets.chomp # save in a variable in case you need to operate on 
-    puts "What color shoes would you prefer?"
+    puts "What type of occasion are you shopping for: Work, Vacation, Special Occasion"
+      occasion = gets.chomp # save in a variable in case you need to operate on
+      # if occasion.downcase == "work"
+      # if occasion.downcase == "vacation"
+      # if occasion.downcase == "special occasion"
+    puts "What color shoes would you prefer: Black, White, Nude, Blue, Red, Yellow, or Multicolor"
       shoe_color = gets.chomp
-    puts "What is your ideal heel height?"
+    puts "What is your ideal heel height (in inches): 3, 4, 5, 6, 7"
       heel_height = gets.chomp
   end 
   
