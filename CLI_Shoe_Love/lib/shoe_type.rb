4d1c02742f12c1540f7a456848efb6825cda31c9
charityyoungblood <youngblood.charity@gmail.com>
2018-01-08 08:03:36 -0800

    added results string to shoe_questions method in user_shoe_preferences.rb

--- CLI_Shoe_Love/lib/shoe_type.rb
+++ CLI_Shoe_Love/lib/shoe_type.rb
@@ -7,10 +7,10 @@ class ShoeType
   def type # this method defines shoe type -- this method may need to be moved to shoes class
     puts "Pumps" # this should be set up to pull pumps based on scrape
     puts "Sandals"
-    puts "Boots"
+    puts "Booties"
     puts "Flats" 
     # determine shoe types to display
-      # boots 
+      # booties 
       # sandals
       # flats 
       # pumps 
--- CLI_Shoe_Love/lib/user_shoe_preferences.rb
+++ CLI_Shoe_Love/lib/user_shoe_preferences.rb
@@ -11,14 +11,25 @@ class Shoes
     puts "What type of occasion are you shopping for: Work, Night Out, Vacation, Special Event"
       occasion = gets.chomp # save in a variable in case you need to operate on
       # if occasion.downcase == "work"
+        # shoe type should include "all types"
+        # Shoes.type should return - Pumps, Sandals, Booties, Flats 
       # if occasion.downcase == "vacation"
-      # if occasion.downcase == "special occasion"
+        # shoe type should not include pumps, booties
+        # Shoes.type should return - Sandals, Flats 
+      # if occasion.downcase == "special event"
+        # shoe type should include pumps, sandals
+        # Shoes.type should return - Pumps, Sandals 
     puts "What color shoes would you prefer: Black, White, Nude, Blue, Red, Yellow, or Multicolor"
       shoe_color = gets.chomp
+      # sort by occasion and then color 
     puts "What is your ideal heel height (in inches): None, 4, 5, 6, 7"
       heel_height = gets.chomp
     puts "What price range do you prefer: $450-$600, $600-$750, $750-$900, $900+"
       shoe_price = gets.chomp
+    puts "Here are your results for #{occasion}: /n
+          1. #{shoe_name} - #{shoe_type} - #{shoe_color} - #{shoe_price}
+          2. #{shoe_name} - #{shoe_type} - #{shoe_color} - #{shoe_price}
+          3. #{shoe_name} - #{shoe_type} - #{shoe_color} - #{shoe_price}"
   end 
   
   
