8be4ec1dba78cba8f970f327bf149f8c196d5675
charityyoungblood <youngblood.charity@gmail.com>
2018-01-08 08:17:36 -0800

    added spec.md file for assessment

--- CLI_Shoe_Love/lib/shoe_type.rb
+++ CLI_Shoe_Love/lib/shoe_type.rb
@@ -5,10 +5,8 @@ class ShoeType
   end 
   
   def type # this method defines shoe type -- this method may need to be moved to shoes class
-    puts "Pumps" # this should be set up to pull pumps based on scrape
-    puts "Sandals"
-    puts "Booties"
-    puts "Flats" 
+    # shoe type should be an array - this way you can iterate over it and delete depending on user preference
+    shoe_type = ["Pumps", "Sandals", "Booties", "Flats"] # this should be set up to pull pumps based on scrape 
     # determine shoe types to display
       # booties 
       # sandals
