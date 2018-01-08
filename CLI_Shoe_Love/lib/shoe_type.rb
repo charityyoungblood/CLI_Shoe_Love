bd095a2af18ac17c82825101ce9c91061314f092
charityyoungblood <youngblood.charity@gmail.com>
2018-01-08 07:33:24 -0800

    added comments for shoe type in shoe_type.rb

--- CLI_Shoe_Love/lib/shoe_type.rb
+++ CLI_Shoe_Love/lib/shoe_type.rb
@@ -4,10 +4,16 @@ class ShoeType
     
   end 
   
-  def type # this method defines shoe type
+  def type # this method defines shoe type -- this method may need to be moved to shoes class
     puts "date night"
     puts "work"
     puts "special occassion"
+    # determine shoe types to display
+      # boots 
+      # sandals
+      # flats 
+      # pumps 
+
   end 
   
   
