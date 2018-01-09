718a84d5430dd4bbb9173bf5011f76882a26eae9
Charity <Youngblood.charity@gmail.com>
2018-01-09 11:29:06 -0800

    tested get_page method

--- CLI_Shoe_Love/lib/scraper.rb
+++ CLI_Shoe_Love/lib/scraper.rb
@@ -38,18 +38,19 @@ class Scraper
   def get_page 
     
     # shoe colors Black, Nude, Blue, Red, Yellow, or Multicolor
-    any_color_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?")
+#    any_color_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?")
     black_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?colori=2")
-    gold_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=16")
-    red_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=12")
-    purple_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=11")
-    
-  #*** for each variable of shoe color - replicate line 50 and 51 **
-    # update the variables to show shoes by color
-#    binding.pry
-    main_page_scrape = Nokogiri::HTML(all_shoes)
-    all_shoes_select = all_shoes.css("div .info_prodotto").text # this currently works 
-    all_shoes_select.split("QUICK VIEW") # this splits the contents of new_shoe (all shoes listed on first page of Aquazurra site into a comma separated array - not sorted by color,price,etc
+#    gold_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=16")
+#    red_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=12")
+#    purple_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=11")
+#    
+#  #*** for each variable of shoe color - replicate line 50 and 51 **
+#    # update the variables to show shoes by color
+##    binding.pry
+#    main_page_scrape = Nokogiri::HTML(all_shoes)
+    black_shoes_scrape = Nokogiri::HTML(open(black_shoes))
+#    all_shoes_select = all_shoes.css("div .info_prodotto").text # this currently works 
+#    all_shoes_select.split("QUICK VIEW") # this splits the contents of new_shoe (all shoes listed on first page of Aquazurra site into a comma separated array - not sorted by color,price,etc
 ##    page.css("a #colori_sel_2")
   end
   
