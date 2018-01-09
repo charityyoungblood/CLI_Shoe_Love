023a26382931f45395090b3c5639b697bc59587d
Charity <Youngblood.charity@gmail.com>
2018-01-09 11:35:15 -0800

    added scrape with color of shoes to scraper.rb

--- CLI_Shoe_Love/lib/scraper.rb
+++ CLI_Shoe_Love/lib/scraper.rb
@@ -38,17 +38,20 @@ class Scraper
   def get_page 
     
     # shoe colors Black, Nude, Blue, Red, Yellow, or Multicolor
-#    any_color_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?")
+    all_color_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?")
     black_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?colori=2")
-#    gold_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=16")
-#    red_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=12")
-#    purple_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=11")
+    gold_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=16")
+    red_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=12")
+    purple_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman.html?colori=11")
 #    
 #  #*** for each variable of shoe color - replicate line 50 and 51 **
 #    # update the variables to show shoes by color
 ##    binding.pry
-#    main_page_scrape = Nokogiri::HTML(all_shoes)
+    all_color_shoes_scrape = Nokogiri::HTML(open(all_color_shoes))
     black_shoes_scrape = Nokogiri::HTML(open(black_shoes))
+    gold_shoes_scrape = Nokogiri::HTML(open( gold_shoes))
+    red_shoes_scrape = Nokogiri::HTML(open(red_shoes))
+    purple_shoes_scrape = Nokogiri::HTML(open(purple_shoes))
 #    all_shoes_select = all_shoes.css("div .info_prodotto").text # this currently works 
 #    all_shoes_select.split("QUICK VIEW") # this splits the contents of new_shoe (all shoes listed on first page of Aquazurra site into a comma separated array - not sorted by color,price,etc
 ##    page.css("a #colori_sel_2")
