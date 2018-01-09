f2eaa19cc9e5e3370040ab0465d26a2e4180d6ab
charityyoungblood <youngblood.charity@gmail.com>
2018-01-08 16:14:40 -0800

    moved previous methods to scraper.rb

--- CLI_Shoe_Love/lib/scraper.rb
+++ CLI_Shoe_Love/lib/scraper.rb
@@ -7,6 +7,28 @@ require_relative './user_shoe_preferences.rb'
 
 class Scraper 
   
+  def user_selections 
+      self.get_courses .each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+      end 
+    end 
+  
+  def get_shoes
+    self.get_page.css(".post")
+  end 
+  
+  def print_user_selections
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+  
   def black_shoes 
     
     browser = Watir::Browser.new
