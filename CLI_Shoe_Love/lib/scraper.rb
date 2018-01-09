a4a6c165601e325dc6bb520b5fabba6e15650b66
charityyoungblood <youngblood.charity@gmail.com>
2018-01-08 16:13:09 -0800

    added placeholders for user_selections, get_shoes and print_user_selections methods in user_shoe_preferences.rb

--- CLI_Shoe_Love/lib/scraper.rb
+++ CLI_Shoe_Love/lib/scraper.rb
@@ -3,16 +3,26 @@ require 'HTTParty'
 require 'nokogiri'
 require 'open-uri'
 require 'watir'
+require_relative './user_shoe_preferences.rb'
 
 class Scraper 
   
   def black_shoes 
     
-#    page = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?")
-    page = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?colori=2")
-    binding.pry
-    page_scrape = Nokogiri::HTML(page)
-#    page.css("a #colori_sel_2")
+    browser = Watir::Browser.new
+
+    browser.goto('https://www.aquazzura.com/en/boutique-online/woman')
+
+    puts browser.title
+
+    browser.close
+    
+    
+##    page = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?")
+#    page = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?colori=2")
+#    binding.pry
+#    page_scrape = Nokogiri::HTML(page)
+##    page.css("a #colori_sel_2")
   end
   
   
--- CLI_Shoe_Love/lib/user_shoe_preferences.rb
+++ CLI_Shoe_Love/lib/user_shoe_preferences.rb
@@ -5,8 +5,38 @@ class Shoes # set shoe type, color, price, heel height
   # access to array of all shoes
   @@all = []
   def initialize 
-    
+    @@all << self 
   end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.reset_all
+    @@all = []
+  end 
+  
+  def user_selections 
+      self.get_courses .each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+      end 
+    end 
+  
+  def get_shoes
+    self.get_page.css(".post")
+  end 
+  
+  def print_user_selections
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
   
   def shoe_questions # this method should prompt the user with questions for shoe type (occasion), color and heel height
     # this method will take in user input and return a list of three selections based on user preferences 
