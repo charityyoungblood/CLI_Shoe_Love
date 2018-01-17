497af1dc8a61c1b021acc74ee154224fdca876b6
charityyoungblood <youngblood.charity@gmail.com>
2018-01-16 20:15:00 -0800

    deleted unless statement from shoe_questions method

--- CLI_Shoe_Love/lib/shoes.rb
+++ CLI_Shoe_Love/lib/shoes.rb
@@ -25,7 +25,7 @@ class Shoes # set shoe type, color, price, heel height
   def shoe_questions # this method should prompt the user with questions for shoe type (occasion), color and heel height
    
     # this method will take in user input and return a list of three selections based on user preferences 
-    puts "What type of occasion are you shopping for: Work, Night Out, Vacation, Special Event"
+    puts "What occasion are you shopping for: Work, Night Out, Vacation, Special Event"
     
       @occasion = gets.chomp # save in a variable in case you need to operate on
     # ***RE-EVALUATE type attribute and how it connects to the program? Do you need a type - shoe name and type are connected once they are displayed*** 
@@ -37,29 +37,37 @@ class Shoes # set shoe type, color, price, heel height
     
     # create a loop or call shoe_questions again so that if the user doesn't enter the correct data, they will be asked the question(s) again
     
-    if occasion.downcase == "work" && @color.downcase == "black" && @heel_height == "6" && @price == "$600"
       black_shoes = HTTParty.get("https://www.aquazzura.com/en/boutique-online/woman/view-all.html?colori=2")
       black_shoes_scrape = Nokogiri::HTML(black_shoes)
       black_shoes_select = black_shoes_scrape.css("div .info_prodotto").text
       selection = black_shoes_select.split("QUICK VIEW")
       
+    
+    if @occasion.downcase == "work" && @type.downcase == "pumps" && @color.downcase == "black" 
+
         @updated_selection = selection.collect do |select|
           select.split("105") 
           # add a regex to take out additional numbers
         end  
-    end
+          puts "I'm here"
           @updated_selection.collect do |shoe_selection|
               shoe_selection.slice!(-1)
           end 
+        puts "I'm here"
+   
+    # ***if statement or unless statement to look for shoe type: Pump, Bootie, Sandal, Flat***
+        
       # *** need to display a string in puts statement below***
      # the puts statement should select 3 random choices, based on user preferences 
-  
+      
+          
       puts "Here are your results for #{occasion}:
       
         1. #{@updated_selection[3]} 
         2. #{@updated_selection[6]}
         3. #{@updated_selection[8]}"
-   
+        end
+  
 
 end
-end 
+end
